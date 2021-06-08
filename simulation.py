f58edf8f92666babc9afda040f7df491346d60e6
Kaustubh Lall <klall@ucsd.edu>
2021-06-08 16:19:14 -0700

    Added basic logic and code skeleton

--- simulation.py
+++ simulation.py
@@ -1,4 +1,4 @@
-from mkl_random.mklrand import choice
+from mkl_random.mklrand import choice, uniform
 from numpy.random.mtrand import sample
 
 from utils import *
@@ -7,7 +7,9 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-INCUBATION_PERIOD = 7  # time before symptoms show up, and you become infectious
+INCUBATION_PERIOD = 5  # time before symptoms show up, and you become infectious
+ACTIVE_DISEASE_PERIOD = INCUBATION_PERIOD + 10  # average duration of incubation + time to recover
+RECOVERY = 0.02  # chance to survive covid
 
 
 class Node:
@@ -19,35 +21,28 @@ class Node:
         self.recovered = recovered
         self.days_exposed = 0  # how many days
 
-    def infect_healthy(self):
-        assert self.alive
-        assert not self.recovered
 
-        # expose self
-        self.exposed = True
-
-    def simulate_day(self):
-        # base conditions
-        if not self.alive:
-            return
-
-        # handle incubation of virus
-        if self.exposed:
-            self.days_exposed += 1
-            # incubation
-            if self.days_exposed > INCUBATION_PERIOD:
-                self.infectious = True
-
-        # handle transmission of virus
-        if self.infectious:
-            self.
-        # handle death chance
-
-        # handle recovery chance
-        pass
+def action_recover_node(g, n):
+    """Logic to recover a node from terminal state"""
+    node = g.nodes[n]
+    node.infectious = False
+    node.exposed = False
+    node.recovered = True
 
 
-def infect_node(g, n):
+def action_kill_node(g, n):
+    """Logic to kill a node which does not survive covid after active duration"""
+    node = g.nodes[n]
+    assert node.alive
+    # death logic
+    node.alive = False
+    node.infectious = False
+    node.exposed = False
+    node.days_exposed = -inf
+
+
+def action_infect_node(g, n):
+    """Logic to infect a node, generally called by neighbors"""
     node = g.nodes[n]
     if node.recovered or not node.alive:
         # ignore nodes that have built an immunity
@@ -57,7 +52,27 @@ def infect_node(g, n):
         node.days_exposed = -1
 
 
+def process_exposed_nodes(g, n):
+    """Logic to process exposed nodes and make them infectious"""
+    node = g.nodes[n]
+    if not node.infectious and node.days_exposed > INCUBATION_PERIOD:
+        node.infectious = True
+
+
+def process_terminal_node(g, n):
+    """Logic to process terminal nodes, aka those at the end of the active disease period"""
+    node = g.nodes[n]
+
+    if node.days_exposed > ACTIVE_DISEASE_PERIOD:
+        # handle recovery and death
+        if uniform() < RECOVERY:
+            action_kill_node(g, n)
+        else:
+            action_recover_node(g, n)
+
+
 def simulate_one_step(g: nx.Graph, r0):
+    """runs one day in a covid-19 simulation"""
     # infect an initial proportion of the population
     n = len(g.nodes)
 

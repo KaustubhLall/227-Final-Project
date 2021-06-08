cea9976e092c6fa28dd1e53e4c4f4c03d8916a36
Kaustubh Lall <klall@ucsd.edu>
2021-06-08 16:27:23 -0700

    Optimized code, added basic simulation code.

--- simulation.py
+++ simulation.py
@@ -24,51 +24,50 @@ class Node:
 
 def action_recover_node(g, n):
     """Logic to recover a node from terminal state"""
-    node = g.nodes[n]
-    node.infectious = False
-    node.exposed = False
-    node.recovered = True
+    (g.nodes[n]).infectious = False
+    (g.nodes[n]).exposed = False
+    (g.nodes[n]).recovered = True
 
 
 def action_kill_node(g, n):
     """Logic to kill a node which does not survive covid after active duration"""
-    node = g.nodes[n]
-    assert node.alive
+    assert g.nodes[n].alive
     # death logic
-    node.alive = False
-    node.infectious = False
-    node.exposed = False
-    node.days_exposed = -inf
+    (g.nodes[n]).alive = False
+    (g.nodes[n]).infectious = False
+    (g.nodes[n]).exposed = False
+    (g.nodes[n]).days_exposed = -inf
 
 
 def action_infect_node(g, n):
     """Logic to infect a node, generally called by neighbors"""
-    node = g.nodes[n]
-    if node.recovered or not node.alive:
+    if g.nodes[n].recovered or not g.nodes[n].alive:
         # ignore nodes that have built an immunity
         return
-    if not node.exposed:
-        node.exposed = True
-        node.days_exposed = -1
+    if not g.nodes[n].exposed:
+        (g.nodes[n]).exposed = True
+        (g.nodes[n]).days_exposed = -1
 
 
 def process_exposed_nodes(g, n):
     """Logic to process exposed nodes and make them infectious"""
-    node = g.nodes[n]
-    if not node.infectious and node.days_exposed > INCUBATION_PERIOD:
-        node.infectious = True
+    if not g.nodes[n].infectious and g.nodes[n].days_exposed > INCUBATION_PERIOD:
+        (g.nodes[n]).infectious = True
 
 
 def process_terminal_node(g, n):
     """Logic to process terminal nodes, aka those at the end of the active disease period"""
-    node = g.nodes[n]
-
-    if node.days_exposed > ACTIVE_DISEASE_PERIOD:
+    if g.nodes[n].days_exposed > ACTIVE_DISEASE_PERIOD:
         # handle recovery and death
         if uniform() < RECOVERY:
             action_kill_node(g, n)
         else:
             action_recover_node(g, n)
+
+
+def prob_interaction(n1, n2):
+    """Probability of a node interacting with another node"""
+    return 1
 
 
 def simulate_one_step(g: nx.Graph, r0):
@@ -77,7 +76,20 @@ def simulate_one_step(g: nx.Graph, r0):
     n = len(g.nodes)
 
     for node in g.nodes:
-        pass
+        # increment day if exposed
+        if node.exposed:
+            node.days_exposed += 1
+
+        # process the node's infection status
+        process_exposed_nodes(g, n)
+
+        # process the node's terminal status
+        process_terminal_node(g, n)
+
+        # process the node's chance to infect its neighbors
+        if node.infectious:
+            [action_infect_node(g, n) for n in node.neighbors() if uniform() < prob_interaction(n, node)]
+
 
 def simulate(r0):
     """Driver function to run a simulation for a given number of days with given input parameters"""
@@ -96,6 +108,7 @@ def generate_barabasi(nodes, edges) -> nx.Graph:
 
     return g_barabasi
 
+
 def generate_eerg(nodes, p) -> nx.Graph:
     """
     Generate a social-network graph to represent a given population.
@@ -105,6 +118,7 @@ def generate_eerg(nodes, p) -> nx.Graph:
     eerg = nx.erdos_renyi_graph(nodes, p)
 
     return eerg
+
 
 def generate_Watts(nodes, knn, p) -> nx.Graph:
     """

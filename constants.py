191bb52caead6c3b82a1310ac839cb2e2111393f
Kaustubh Lall <klall@ucsd.edu>
2021-06-09 16:59:22 -0700

    debug graphs added

--- constants.py
+++ constants.py
@@ -6,4 +6,4 @@ INCUBATION_PERIOD = 5  # time before symptoms show up, and you become infectious
 ACTIVE_DISEASE_PERIOD = INCUBATION_PERIOD + 10  # average duration of incubation + time to recover
 RECOVERY = 0.02  # chance to survive covid
 CHANCE_TRANSMISSION_INFECTED = 0.05  # accounting for quarantine
-CHANCE_TRANSMISSION_EXPOSED = 0.3  #
+CHANCE_TRANSMISSION_EXPOSED = 0.2  #
